023795ae7bfb1f0c162c2f115ab8ecbb57780811
Dmitry Vyatkin <d.vyatkin@corp.mail.ru>
2021-03-24 19:14:32 +0300

    init

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-// содежимое index.js
-const http = require('http')
-const port = 80
-const requestHandler = (request, response) => {
-  console.log(request.url)
-  response.end('Hello Node.js Server!')
-}
-const server = http.createServer(requestHandler)
-server.listen(port, (err) => {
+const express = require('express')
+const app = express()
+const port = 3000
+app.get('/', (request, response) => {
+response.send('Hello from Express!')
+})
+
+app.listen(port, (err) => {
   if (err) {
     return console.log('something bad happened', err)
   }

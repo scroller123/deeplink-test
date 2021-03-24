a9400b3f653b4fda7ead48edc2588dae068b6d95
Dmitry Vyatkin <d.vyatkin@corp.mail.ru>
2021-03-24 20:33:45 +0300

    init

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
-const port = 80
+const port = process.env.PORT
+
 app.get('/', (request, response) => {
 response.send('Hello from Express!')
 })
